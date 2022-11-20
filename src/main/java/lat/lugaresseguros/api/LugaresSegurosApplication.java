25c34f7b342dc9c7fa7344d7e5f3dc1b5e905f48
irmalop <ada_wit18@hackademy.mx>
2022-11-20 14:36:13 -0600

    Created project

---
+++ src/main/java/lat/lugaresseguros/api/LugaresSegurosApplication.java
@@ -0,0 +1,13 @@
+package lat.lugaresseguros.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LugaresSegurosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LugaresSegurosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lat/lugaresseguros/api/LugaresSegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package lat.lugaresseguros.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LugaresSegurosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
